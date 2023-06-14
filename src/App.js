04764dd19e3a8c765e300b6bbfef093e9870ecaf
jeonmye <jeonmye@gmail.com>
2023-06-14 20:50:59 +0900

    commit 3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 import { Button, Navbar, Container, Nav } from "react-bootstrap";
 import { Row, Col } from "react-bootstrap";
+import burgerData from "./data.js";
 
 function App() {
+  let [burger] = useState(burgerData);
+
   return (
     <div className="App">
       <Navbar bg="warning" variant="light" className="nav">
@@ -26,8 +29,9 @@ function App() {
               src={process.env.PUBLIC_URL + "/img/빅맥.png"}
               width="80%"
             ></img>
-            <h4>상품명</h4>
-            <p>상품설명</p>
+            <h4>{burgerData[0].title}</h4>
+            <p>{burgerData[0].content}</p>
+            <p>{burgerData[0].price}</p>
           </Col>
           <Col sm>
             <img
---
+++ src/data.js
@@ -0,0 +1,26 @@
+let burgerData = [
+  {
+    id: 0,
+    title: "빅맥",
+    content:
+      "100% 순 쇠고기 패티 두 장에 빅맥®만의 특별한 소스. 입안에서 살살 녹는 치즈와 신선한 양상추, 양파, 그리고 피클까지.50년 넘게 전 세계인의 입맛을 사로 잡은 버거의 대명사.",
+
+    price: "6400원",
+  },
+
+  {
+    id: 1,
+    title: "슈슈버거",
+    content: "Born in Seoul",
+    price: 110000,
+  },
+
+  {
+    id: 2,
+    title: "맥스파이시 버거",
+    content: "Born in the States",
+    price: 130000,
+  },
+];
+
+export default burgerData;
